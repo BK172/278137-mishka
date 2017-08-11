56b1fafc5c099812bd2cfe059b05684ae1f21fc8
dsvksd <dsvksdvs@jdns.com>
2017-08-11 14:16:02 +0300

    Добавил обработку закрытия модального окна, поправил скрипты

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
 (function(){
   var popup = document.querySelector('.popup');
   var mainOrderBtn = document.querySelector('.btn--main-order');
-  var basketBtn = document.querySelectorAll('.product-card__basket');
+  var basketBtns = document.querySelectorAll('.product-card__basket');
+
   var orderHandler = function (evt) {
     evt.preventDefault();
-    popup.classList.toggle('hidden');
+    popup.classList.remove('hidden');
+    document.body.addEventListener('keydown', escPressHandler);
+    document.body.addEventListener('click', documentClickHandler);
   };
 
-  if (basketBtn) {
-    basketBtn.forEach(function (item) {
-      item.addEventListener('click', orderHandler);
-    });
+  var escPressHandler = function (evt) {
+    if (evt.keyCode === 27) {
+      closeOrderDialog();
+    }
+  };
+
+  var documentClickHandler = function (evt) {
+    if (evt.target.classList.contains('popup')) {
+      closeOrderDialog();
+    }
+  };
+
+  function closeOrderDialog() {
+    document.body.removeEventListener('keydown', escPressHandler);
+    document.body.removeEventListener('click', documentClickHandler);
+    popup.classList.add('hidden');
+  }
+
+  if (basketBtns) {
+    for (var i = 0, len = basketBtns.length; i < len; i++) {
+      basketBtns[i].addEventListener('click', orderHandler);
+    }
   }
 
   if (mainOrderBtn) {
@@ -19,46 +40,49 @@
 
   var navToggler = document.querySelector('.main-nav__toggle');
   var elements = document.querySelectorAll('.main-nav__mobile-item');
+
   var mainNavHandler = function() {
     var menuState = elements[0].classList.contains('main-nav--closed');
 
     if (menuState) {
-      [].forEach.call(elements, function (item) {
-        item.classList.remove('main-nav--closed');
-      });
+      for (var i = 0, len = elements.length; i < len; i++) {
+        elements[i].classList.remove('main-nav--closed');
+      }
       navToggler.classList.add('main-nav__toggle--closed');
     } else {
-      [].forEach.call(elements, function (item) {
-        item.classList.add('main-nav--closed');
-      });
+      for (var i = 0, len = elements.length; i < len; i++) {
+        elements[i].classList.add('main-nav--closed');
+      }
       navToggler.classList.remove('main-nav__toggle--closed');
     }
   };
 
-  [].forEach.call(elements, function (item) {
-    item.classList.add('main-nav--closed');
-  });
+  for (var i = 0, len = elements.length; i < len; i++) {
+    elements[i].classList.add('main-nav--closed');
+  }
 
   navToggler.addEventListener('click', mainNavHandler);
 
-  var staticMap = document.querySelector(".main-map > picture");
+  var staticMap = document.querySelector('.main-map > picture');
 
-  ymaps.ready(function () {
-    staticMap.style.display = "none";
-    var myMap = new ymaps.Map("YMapsID", {
-      center: [59.936354, 30.321670],
-      zoom: 16
+  if (staticMap) {
+    ymaps.ready(function () {
+      staticMap.style.display = 'none';
+      var myMap = new ymaps.Map('YMapsID', {
+        center: [59.936354, 30.321670],
+        zoom: 16
+      });
+      var myPlacemark = new ymaps.Placemark([59.936354, 30.321670], {}, {
+        iconImageHref: '../img/icon-map-pin.svg',
+        iconImageSize: [66, 101],
+        iconImageOffset: [-39, -100],
+        hintContent: 'Мишка!',
+            balloonContent: 'г. Санкт-Петербург,<br />' +
+          ' ул. Большая Конюшенная,<br />' +
+          'д.19/8, офис 101'
+      });
+      myMap.behaviors.disable('scrollZoom');
+      myMap.geoObjects.add(myPlacemark);
     });
-    var myPlacemark = new ymaps.Placemark([59.936354, 30.321670], {}, {
-      iconImageHref: '../img/icon-map-pin.svg',
-      iconImageSize: [66, 101],
-      iconImageOffset: [-39, -100],
-      hintContent: 'Мишка!',
-          balloonContent: 'г. Санкт-Петербург,<br />' +
-        ' ул. Большая Конюшенная,<br />' +
-        'д.19/8, офис 101'
-    });
-    myMap.behaviors.disable("scrollZoom");
-    myMap.geoObjects.add(myPlacemark);
-  });
+  }
 }());
